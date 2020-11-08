1c887ec28f070d0b7d7ecbf7ab9b01162d433946
Katherine <manserka@oregonstate.edu>
2020-11-08 18:14:04 -0500

    profile/:id works now to render courses and skills but with duplicates

--- main.js
+++ main.js
@@ -49,11 +49,25 @@ function getProfileSelected(res, req, mysql, context, complete){
    }
 **/
 
-/** Get a selected profile based on a given profile_id **/
-/** Works with fields in Jeff's DB: **/
-function getProfileSelected(res, req, mysql, context, complete){
-     var sql = "SELECT profile_id as profile_id, first_name as first_name, last_name as last_name, email as email, industry as industry, github_link as github_link, linkedin_link as linkedin_link, twitter_link as twitter_link FROM Profiles WHERE profile_id=?";
+// /** Get a selected profile based on a given profile_id **/
+// /** Works with fields in Jeff's old DB: **/
+// function getProfileSelected(res, req, mysql, context, complete){
+//      var sql = "SELECT profile_id as profile_id, first_name as first_name, last_name as last_name, email as email, industry as industry, github_link as github_link, linkedin_link as linkedin_link, twitter_link as twitter_link FROM Profiles WHERE profile_id=?";
+//      var selectedId = req.params.id;
+//      mysql.pool.query(sql, selectedId, function(error, results, fields){
+//          if(error){
+//              res.write(JSON.stringify(error));
+//              res.end();
+//          }
+//          context.profile  = results;
+//          complete();
+//      });
+//   }
+
+// Trying to get all profile fields with one query
+ function getProfileSelected(res, req, mysql, context, complete){
      var selectedId = req.params.id;
+     var sql = "SELECT * FROM Profiles_Skills PS INNER JOIN Profiles P on PS.profile_id = P.profile_id INNER JOIN Skills S on PS.skill_id = S.skill_id INNER JOIN Profiles_Courses PC on P.profile_id = PC.profile_id INNER JOIN Courses C on PC.course_id = C.course_id WHERE PS.profile_id = ?;";
      mysql.pool.query(sql, selectedId, function(error, results, fields){
          if(error){
              res.write(JSON.stringify(error));
@@ -63,6 +77,41 @@ function getProfileSelected(res, req, mysql, context, complete){
          complete();
      });
   }
+
+// Trying query to get all profile fields with multiple queries
+// function getProfileSelected(res, req, mysql, context, complete){
+//     //does not work - duplicates data: var sql = "SELECT * FROM Profiles_Skills PS INNER JOIN Profiles P on PS.profile_id = P.profile_id INNER JOIN Skills S on PS.skill_id = S.skill_id INNER JOIN Profiles_Courses PC on P.profile_id = PC.profile_id INNER JOIN Courses C on PC.course_id = C.course_id WHERE PS.profile_id = 1;";
+//      var selectedId = req.params.id;
+//      var sql1 = "SELECT profile_id as profile_id, first_name as first_name, last_name as last_name, email as email, industry as industry, github_link as github_link, linkedin_link as linkedin_link, twitter_link as twitter_link FROM Profiles WHERE profile_id=?";
+//      mysql.pool.query(sql1, selectedId, function(error, results, fields){
+//          if(error){
+//              res.write(JSON.stringify(error));
+//              res.end();
+//          }
+//          context.profile  = results;
+//          //complete();
+//      });
+//
+//      var sql2 = "SELECT skill_name FROM Skills S INNER JOIN Profiles_Skills PS ON S.skill_id = PS.skill_id Where PS.profile_id = ?;"
+//      mysql.pool.query(sql2, selectedId, function(error, results, fields){
+//          if(error){
+//              res.write(JSON.stringify(error));
+//              res.end();
+//          }
+//          context.skills  = results;
+//          //complete();
+//      });
+//
+//      var sql3 = "SELECT course_name FROM Courses C INNER JOIN Profiles_Courses PC ON C.course_id = PC.course_id Where PC.profile_id = ?;"
+//      mysql.pool.query(sql3, selectedId, function(error, results, fields){
+//          if(error){
+//              res.write(JSON.stringify(error));
+//              res.end();
+//          }
+//          context.courses  = results;
+//          complete();
+//      });
+//   }
 
 app.get('/', (req, res) => {
    res.redirect('homepage');
