44e9f2d8f3ea8afdb2783cc620a12db3680ac3dc
JBPort94 <49971764+JBPort94@users.noreply.github.com>
2020-11-08 19:54:39 -0700

    Update dbcon.js - Correct hostname

--- dbcon.js
+++ dbcon.js
@@ -2,7 +2,7 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'mysql.eecs.oregonstate.edu',
+  host            : 'classmysql.engr.oregonstate.edu',
   user            : 'cs361_portejef',
   password        : '4220',
   database        : 'cs361_portejef'
